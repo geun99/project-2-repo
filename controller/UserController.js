a9bbfbcecd910c1aa9134fcc748d2bd61a4028ab
geun99 <lich1115@naver.com>
2024-01-02 08:51:54 +0900

    로그인 구현

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,6 +1,8 @@
 const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
-
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 const join = (req, res) => {
   const { email, password } = req.body;
 
@@ -17,7 +19,35 @@ const join = (req, res) => {
 };
 
 const login = (req, res) => {
-  res.json("로그인");
+  const { email, password } = req.body;
+
+  let sql = "SELECT * FROM users WHERE email = ?";
+  conn.query(sql, email, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    const loginUser = results[0];
+    if (loginUser && loginUser.password == password) {
+      const token = jwt.sign(
+        {
+          email: loginUser.email,
+        },
+        process.env.PRIVATE_KEY,
+        {
+          expiresIn: "5m",
+          issuer: "geun99",
+        }
+      );
+      res.cookie("token", token, {
+        httpOnly: true,
+      });
+      console.log(token);
+      return res.status(StatusCodes.OK).json(results);
+    } else {
+      return res.status(StatusCodes.UNAUTHORIZED).json(results);
+    }
+  });
 };
 
 const passwordResetRequest = (req, res) => {
