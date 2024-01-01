8a08403db2762946572639ae4e7040ab73cb18dd
geun99 <lich1115@naver.com>
2024-01-02 06:45:07 +0900

    필요 모듈 설치, app.js, routes, mariadb.js 생성 및 기초 틀 짜기 완료

---
+++ routes/orders.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+router.use(express.json());
+
+router.post("/", (req, res) => {
+  res.json("주문 하기");
+});
+
+router.get("/", (req, res) => {
+  res.json("주문 조회");
+});
+
+router.get("/:id", (req, res) => {
+  res.json("주문 상세 조회");
+});
+
+module.exports = router;
