8e3d2ef1f32e2cc3fc64747f9f90e84c94622405
geun99 <lich1115@naver.com>
2024-01-02 08:35:00 +0900

    controller routes 분리

--- routes/users.js
+++ routes/users.js
@@ -1,34 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const conn = require("../mariadb");
+const { StatusCodes } = require("http-status-codes");
+const {
+  join,
+  login,
+  passwordResetRequest,
+  passwordReset,
+} = require("../controller/UserController");
 
 router.use(express.json());
 
-router.post("/join", (req, res) => {
-  const { email, password } = req.body;
-
-  let sql = "INSERT INTO users (email, password) VALUES (?, ?)";
-  let values = [email, password];
-
-  conn.query(sql, values, (err, results) => {
-    if (err) {
-      console.log(err);
-      return res.status(400).end();
-    }
-    res.status(201).json(results);
-  });
-});
-
-router.post("/login", (req, res) => {
-  res.json("로그인");
-});
-
-router.post("/reset", (req, res) => {
-  res.json("비밀번호 초기화 요청");
-});
-
-router.put("/reset", (req, res) => {
-  res.json("비밀번호 초기화");
-});
+router.post("/join", join);
+router.post("/login", login);
+router.post("/reset", passwordResetRequest);
+router.put("/reset", passwordReset);
 
 module.exports = router;
