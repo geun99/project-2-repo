0931e4c587bab9163d03c3f0b2f5063d00b31dd9
geun99 <lich1115@naver.com>
2024-01-03 03:53:45 +0900

    BookController 분리 및 전체 도서 조회, 개별 도서 조회 구현

---
+++ controller/BookController.js
@@ -0,0 +1,37 @@
+const conn = require("../mariadb");
+const { StatusCodes } = require("http-status-codes");
+
+const allBooks = (req, res) => {
+  let sql = `SELECT * FROM books`;
+  conn.query(sql, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    return res.status(StatusCodes.OK).json(results);
+  });
+};
+
+const bookDetail = (req, res) => {
+  let { id } = req.params;
+
+  let sql = `SELECT * FROM books WHERE id = ?`;
+  conn.query(sql, id, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    if (results[0]) return res.status(StatusCodes.OK).json(results[0]);
+    else return res.status(StatusCodes.NOT_FOUND).end();
+  });
+};
+
+const booksByCategory = (req, res) => {
+  res.json("카테고리별 도서 목록 조회");
+};
+
+module.exports = {
+  allBooks,
+  bookDetail,
+  booksByCategory,
+};
